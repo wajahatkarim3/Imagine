da93c2b70c65a690d0fd130c8375de5fac602d09
Wajahat Karim <wajahatkarim3@gmail.com>
2021-03-16 16:00:31 +0500

    Danger integration in CI continue

---
+++ Dangerfile.df.kts
@@ -0,0 +1,33 @@
+import systems.danger.kotlin.*
+
+danger(args) {
+
+    val allSourceFiles = git.modifiedFiles + git.createdFiles
+    val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
+
+    onGitHub {
+        message("Thanks @#{github.pr_author} 👍")
+        // val isTrivial = pullRequest.title.contains("#trivial")
+
+        // Changelog
+        //if (!isTrivial && !changelogChanged && sourceChanges != null) {
+        //    warn(WordUtils.capitalize("any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md)."))
+        //}
+
+        // Encourage contributors to write useful descriptions
+        if (pullRequest.body.length < 20) {
+            warn("Please provide a Pull Request description ...")
+        }
+
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+
+        // Work in progress check
+        if (pullRequest.title.contains("WIP", false)) {
+            warn("PR is classed as Work in Progress")
+        }
+    }
+}
--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem 'danger'
-gem 'danger-checkstyle_format'
+gem "danger"
+gem "danger-checkstyle_format"
