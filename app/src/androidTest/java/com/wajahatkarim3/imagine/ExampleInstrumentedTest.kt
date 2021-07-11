62470c93ad83c88ec19deffd8c29e2d9c49d0d82
Wajahat Karim <wajahat@airlifttech.com>
2021-07-11 21:39:15 +0500

    Added the fastlane and screengrab.

--- app/src/androidTest/java/com/wajahatkarim3/imagine/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/wajahatkarim3/imagine/ExampleInstrumentedTest.kt
@@ -15,11 +15,20 @@
 */
 package com.wajahatkarim3.imagine
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.wajahatkarim3.imagine.ui.MainActivity
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -28,10 +37,31 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    // JVMField needed!
+    @Rule @JvmField
+    val localeTestRule = LocaleTestRule()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java, false, false)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.wajahatkarim3.imagine", appContext.packageName)
     }
+
+    @Test
+    fun testTakeScreenshot() {
+        activityRule.launchActivity(null)
+        //1
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerTags))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        //2
+        Screengrab.screenshot("photosList")
+    }
+
 }
---
+++ fastlane/README.md
@@ -0,0 +1,39 @@
+fastlane documentation
+================
+# Installation
+
+Make sure you have the latest version of the Xcode command line tools installed:
+
+```
+xcode-select --install
+```
+
+Install _fastlane_ using
+```
+[sudo] gem install fastlane -NV
+```
+or alternatively using `brew install fastlane`
+
+# Available Actions
+## Android
+### android test
+```
+fastlane android test
+```
+Runs all the tests
+### android build
+```
+fastlane android build
+```
+Build
+### android build_for_screengrab
+```
+fastlane android build_for_screengrab
+```
+Build debug and test APK for screenshots
+
+----
+
+This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
+More information about fastlane can be found on [fastlane.tools](https://fastlane.tools).
+The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
